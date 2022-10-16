70a6cb26034a6791fc67ea48be3683836f1b9094
Praveen <77868484+praveen106@users.noreply.github.com>
2022-10-16 11:44:09 +0530

    Add files via upload

---
+++ payment.js
@@ -0,0 +1,16 @@
+document.getElementById("delivery-info").addEventListener('click',addFunction)
+   function addFunction(){
+      location.href = "filldetails.html"
+      
+      
+   }
+
+   document.getElementById("logo").addEventListener("click",logofunction);
+
+   function logofunction() {
+      location.href = "homepage.html";
+   }
+
+
+
+
